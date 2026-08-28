46a2f1e4531e2ca04ad89dc409ce5a9d3b1c852b
PEDIHS <130468444+PEDIHS@users.noreply.github.com>
2026-08-28 07:40:22 +0330

    Phase 7 Android auth: remove redundant vault boundary

--- apps/android/core/control-api/src/main/java/com/ganj/vpn/core/controlapi/AuthSessionVault.kt
+++
@@ -1,7 +0,0 @@
-package com.ganj.vpn.core.controlapi
-
-interface AuthSessionVault {
-    fun restore(): AuthSessionCredentials?
-    fun save(session: AuthSessionCredentials): Result<Unit>
-    fun clear(): Result<Unit>
-}
